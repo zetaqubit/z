ba8fe7257a39bdba638bd3b8e64aaddfc4be14b9
z <z@z.com>
2015-06-15 22:44:04 -0700

    Rename hello to project name.

---
+++ src/genesis/visualizer.h
@@ -0,0 +1,16 @@
+#ifndef SRC_GENESIS_VISUALIZER_H_
+#define SRC_GENESIS_VISUALIZER_H_
+
+namespace genesis {
+
+class Visualizer {
+ public:
+  void Init();
+  void Run();
+
+ private:
+};
+
+}  // namespace genesis
+
+#endif  // SRC_GENESIS_VISUALIZER_H_
