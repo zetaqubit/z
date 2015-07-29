fb753ba21d879f863c182e370ee8ee26e6f3ecac
z <z@z.com>
2015-07-28 20:32:05 -0700

    Muck with caffe build

--- src/third_party/caffe/src/caffe/common.cpp
+++ src/third_party/caffe/src/caffe/common.cpp
@@ -32,7 +32,7 @@ int64_t cluster_seedgen(void) {
 
 void GlobalInit(int* pargc, char*** pargv) {
   // Google flags.
-  ::gflags::ParseCommandLineFlags(pargc, pargv, true);
+  //::gflags::ParseCommandLineFlags(pargc, pargv, true);
   // Google logging.
   ::google::InitGoogleLogging(*(pargv)[0]);
   // Provide a backtrace on segfault.
