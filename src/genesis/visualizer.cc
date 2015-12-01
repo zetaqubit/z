ce6b7b37ba3c9bde147b85f6e18d17355d0e0638
Zhao <x.zhao.y@gmail.com>
2015-11-30 22:49:43 -0800

    Change visualization to orthographic projection, and match the visualization fov to that of the camera.

--- src/genesis/visualizer.cc
+++ src/genesis/visualizer.cc
@@ -14,8 +14,9 @@ namespace genesis {
 
 namespace {
 
-static const int kWindowWidth = 1024;
-static const int kWindowHeight = 768;
+static const int kWindowWidth = 800;
+static const int kAspect = 1.1478;  // Leap device has fov of 2.304 x 2.007
+static const int kWindowHeight = kWindowWidth / kAspect;
 static const string kVertexShaderFile = "src/genesis/data/vertex.glsl";
 static const string kFragmentShaderFile = "src/genesis/data/fragment.glsl";
 
@@ -120,8 +121,9 @@ bool Visualizer::InitScene() {
   // Setup projection and view matrices.
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  SetPerspectiveFrustum(110 /* fov */, 2 * kWindowWidth / kWindowHeight,
-      1 /* near_plane */, 20 /* far_plane */);
+  //SetPerspectiveFrustum(132 /* fov */, kWindowWidth / kWindowHeight,
+  //    1 /* near_plane */, 20 /* far_plane */);
+  glOrtho(-1, 1, -1, 1, -1, 1);
   glMatrixMode(GL_MODELVIEW);
 
   return true;
@@ -131,6 +133,9 @@ void Visualizer::Run() {
   cout << "Let there be light!" << endl;
 
   SDL_Event event;
+
+  LOG(INFO) << "Camera fov: " << controller_->devices()[0].horizontalViewAngle()
+      << " x " << controller_->devices()[0].verticalViewAngle();
 
   while (should_run_) {
     while (SDL_PollEvent(&event) != 0) {
@@ -193,18 +198,18 @@ void Visualizer::Update() {
 
 void Visualizer::Render() {
   SDL_RenderClear(renderer_.get());
-  glClearColor(1.f, 1.f, 1.f, 1.f);
+  glClearColor(0.f, 1.f, 1.f, 1.f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  glTranslatef(0., 0., -10.);
+  //glTranslatef(0., 0., -10.);
 
   // Move sensor images away from camera and scale to maintain distance-to-size
   // ratio of 4:1.
   glPushMatrix();
-  glScalef(80., 80., 1.);
-  glTranslatef(0., 0., -10.);
+  //glScalef(50., 50., 1.);
+  //glTranslatef(0., 0., -10.);
 
   glEnable(GL_TEXTURE_2D);
   glUseProgram(program_);
@@ -215,10 +220,11 @@ void Visualizer::Render() {
   glActiveTexture(GL_TEXTURE1);
   glBindTexture(GL_TEXTURE_2D, distortion_left_texture_);
   glBegin(GL_QUADS);
-  glTexCoord2f(0.f, 1.f); glVertex3f(-1.f,  1.f, 0.f); // Top Left
-  glTexCoord2f(1.f, 1.f); glVertex3f( 1.f,  1.f, 0.f); // Top Right
-  glTexCoord2f(1.f, 0.f); glVertex3f( 1.f, -1.f, 0.f); // Bottom Right
-  glTexCoord2f(0.f, 0.f); glVertex3f(-1.f, -1.f, 0.f); // Bottom Left
+  float a = 1 / kAspect;
+  glTexCoord2f(0.f, 1.f); glVertex3f(-1.f,  a, 0.f); // Top Left
+  glTexCoord2f(1.f, 1.f); glVertex3f( 1.f,  a, 0.f); // Top Right
+  glTexCoord2f(1.f, 0.f); glVertex3f( 1.f, -a, 0.f); // Bottom Right
+  glTexCoord2f(0.f, 0.f); glVertex3f(-1.f, -a, 0.f); // Bottom Left
   glEnd();
 
   glPopMatrix();
