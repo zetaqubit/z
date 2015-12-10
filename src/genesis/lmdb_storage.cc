fd292d801012f121ebe8780e3353cc40fb0fb498
Zhao <x.zhao.y@gmail.com>
2015-12-10 02:17:24 -0800

    Add utility for taking saved protos and storing them in LMDB.

---
+++ src/genesis/lmdb_storage.cc
@@ -0,0 +1,99 @@
+#include "lmdb_storage.h"
+
+#include <stdint.h>
+#include <sys/stat.h>
+#include <glog/logging.h>
+
+#include "src/third_party/caffe/src/caffe/proto/caffe.pb.h"
+
+namespace genesis {
+
+using caffe::Datum;
+
+LmdbStorage::LmdbStorage(const std::string& db_path, WriteMode mode) {
+  // Create LMDB directory if it does not already exist.
+  LOG(INFO) << "Opening lmdb " << db_path;
+  int mkdir_failed = mkdir(db_path.c_str(), 0744);
+
+  if (mode == OVERWRITE) {
+    LOG(INFO) << "Overwriting database.";
+    if (mkdir_failed) {
+      LOG(FATAL) << "Could not overwrite: " << db_path << " already exists.";
+      return;
+    }
+  } else if (mode == APPEND) {
+    LOG(INFO) << "Appending to database.";
+  }
+
+  CHECK_EQ(mdb_env_create(&mdb_env_), MDB_SUCCESS) << "mdb_env_create failed";
+  CHECK_EQ(mdb_env_set_mapsize(mdb_env_, 1099511627776), MDB_SUCCESS)  // 1TB
+      << "mdb_env_set_mapsize failed";
+  CHECK_EQ(mdb_env_open(mdb_env_, db_path.c_str(), 0, 0664), MDB_SUCCESS)
+      << "mdb_env_open failed";
+  CHECK_EQ(mdb_txn_begin(mdb_env_, NULL, 0, &mdb_txn_), MDB_SUCCESS)
+      << "mdb_txn_begin failed";
+  CHECK_EQ(mdb_open(mdb_txn_, NULL, 0, &mdb_dbi_), MDB_SUCCESS)
+      << "mdb_open failed. Does the lmdb already exist? ";
+}
+
+LmdbStorage::~LmdbStorage() {
+  CHECK_EQ(mdb_txn_commit(mdb_txn_), MDB_SUCCESS) << "mdb_txn_commit failed";
+  mdb_close(mdb_env_, mdb_dbi_);
+  mdb_env_close(mdb_env_);
+}
+
+bool LmdbStorage::Write(uint32_t key, const proto::LeapFrame& proto) {
+  int width = proto.left().width();
+  int height = proto.left().height();
+  bool has_hand = proto.has_hand_pose();
+
+  Datum datum;
+  datum.set_channels(1);
+  datum.set_width(width);
+  datum.set_height(height);
+
+  datum.mutable_float_data()->CopyFrom(proto.left().data());
+  datum.set_label(has_hand ? 1 : 0);
+
+  const int kMaxKeyLength = 10;
+  char key_cstr[kMaxKeyLength];
+  std::string value;
+  snprintf(key_cstr, kMaxKeyLength, "%08d", key);
+  datum.SerializeToString(&value);
+  std::string keystr(key_cstr);
+
+
+  // Write in database
+  MDB_val mdb_key, mdb_data;
+  mdb_data.mv_size = value.size();
+  mdb_data.mv_data = reinterpret_cast<void*>(&value[0]);
+  mdb_key.mv_size = keystr.size();
+  mdb_key.mv_data = reinterpret_cast<void*>(&keystr[0]);
+  if (mdb_put(mdb_txn_, mdb_dbi_, &mdb_key, &mdb_data, 0) != MDB_SUCCESS) {
+    LOG(ERROR) << "mdb_put failed";
+    return false;
+  }
+
+  LOG(INFO) << "Wrote item: " << key;
+
+  if (key % 100 == 0) {
+    if (!Flush()) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool LmdbStorage::Flush() {
+  if (mdb_txn_commit(mdb_txn_) != MDB_SUCCESS) {
+    LOG(ERROR) << "mdb_txn_commit failed";
+    return false;
+  }
+  if (mdb_txn_begin(mdb_env_, NULL, 0, &mdb_txn_) != MDB_SUCCESS) {
+    LOG(ERROR) << "mdb_txn_begin failed";
+    return false;
+  }
+  return true;
+}
+
+}  // namespace genesis
---
+++ src/genesis/proto_to_lmdb_converter_main.cc
@@ -0,0 +1,47 @@
+#include "src/genesis/proto_to_lmdb_converter.h"
+
+#include <stdint.h>
+#include <string>
+#include <glog/logging.h>
+
+namespace {
+
+static const std::string kProtoDir = 
+    "/home/z/hand_tracking/blaze_root/src/genesis/data/proto";
+static const std::string kLmdbDir =
+    "/home/z/hand_tracking/blaze_root/src/genesis/data/lmdb";
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  google::InitGoogleLogging(argv[0]);
+  FLAGS_logtostderr = 1;
+
+  if (argc < 4) {
+    LOG(FATAL) << "Usage: " << argv[0]
+        << " [APPEND|OVERWRITE] starting_number ending_number";
+    return 1;
+  }
+
+  genesis::LmdbStorage::WriteMode mode;
+  if (!std::string("APPEND").compare(argv[1])) {
+    mode = genesis::LmdbStorage::APPEND;
+  } else if (!std::string("OVERWRITE").compare(argv[1])) {
+    mode = genesis::LmdbStorage::OVERWRITE;
+  } else {
+    LOG(FATAL) << "Invalid write mode: " << argv[1]
+        << ". Valid options: APPEND, OVERWRITE.";
+  }
+
+  int start = std::stoi(argv[2]);
+  int end = std::stoi(argv[3]);
+  if (start < 0 || end < 0 || start > end) {
+    LOG(FATAL) << "Require start >= 0, end >=0, and start <= end.";
+    return 1;
+  }
+
+  genesis::ProtoToLmdbConverter converter(kProtoDir, kLmdbDir, mode);
+  converter.Convert(static_cast<uint32_t>(start),
+                    static_cast<uint32_t>(end));
+  return 0;
+}
