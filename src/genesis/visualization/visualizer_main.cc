bc3ce7ab5e506e5a8f6bcd176b0134c7a9262249
Zhao <x.zhao.y@gmail.com>
2016-01-08 14:48:10 -0800

    Support loading a model snapshot in visualizer via --model.

--- src/genesis/visualization/visualizer_main.cc
+++ src/genesis/visualization/visualizer_main.cc
@@ -2,12 +2,15 @@
 
 #include <string>
 
+#include "src/third_party/gflags/include/gflags/gflags.h"
 #include "src/third_party/glog/src/glog/logging.h"
 #include "src/third_party/leap_motion/include/Leap.h"
 
-int main(int /* argc */, char** argv) {
+int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
   FLAGS_logtostderr = 1;
+
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
 
   Leap::Controller controller;
   while (!controller.isConnected()) {
